46c5e596cc213c7239cefdf6183b5acec4a9fa05
wukewei <wukewei@le5le.com>
2024-06-14 16:41:10 +0800

    u

--- gga.go
+++ gga.go
@@ -20,40 +20,43 @@ const (
 )
 
 // GGA is the Time, position, and fix related data of the receiver.
-// http://aprs.gids.nl/nmea/#gga
-// https://gpsd.gitlab.io/gpsd/NMEA.html#_gga_global_positioning_system_fix_data
-//
-// Format: $--GGA,hhmmss.ss,ddmm.mm,a,ddmm.mm,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh<CR><LF>
-// Example: $GNGGA,203415.000,6325.6138,N,01021.4290,E,1,8,2.42,72.5,M,41.5,M,,*7C
 type GGA struct {
 	BaseSentence
-	Time          Time    // Time of fix.
-	Latitude      float64 // Latitude.
-	Longitude     float64 // Longitude.
-	FixQuality    string  // Quality of fix.
-	NumSatellites int64   // Number of satellites in use.
-	HDOP          float64 // Horizontal dilution of precision.
-	Altitude      float64 // Altitude.
-	Separation    float64 // Geoidal separation
-	DGPSAge       string  // Age of differential GPD data.
-	DGPSId        string  // DGPS reference station ID.
+	Time             Time    // Time of fix.
+	Latitude         float64 // Latitude.
+	NorS             string  // N or S.
+	Longitude        float64 // Longitude.
+	EorW             string  // E or W.
+	FixQuality       string  // Quality of fix.
+	NumSatellites    int64   // Number of satellites in use.
+	HDOP             float64 // Horizontal dilution of precision.
+	Altitude         float64 // Altitude.
+	AntennaMeters    string
+	Separation       float64 // Geoidal separation
+	SeparationMeters string
+	DGPSAge          string // Age of differential GPD data.
+	DGPSId           string // DGPS reference station ID.
 }
 
 // newGGA constructor
-func newGGA(s BaseSentence) (Sentence, error) {
+func newGGA(s BaseSentence) (GGA, error) {
 	p := NewParser(s)
 	p.AssertType(TypeGGA)
 	return GGA{
-		BaseSentence:  s,
-		Time:          p.Time(0, "time"),
-		Latitude:      p.LatLong(1, 2, "latitude"),
-		Longitude:     p.LatLong(3, 4, "longitude"),
-		FixQuality:    p.EnumString(5, "fix quality", Invalid, GPS, DGPS, PPS, RTK, FRTK, EST),
-		NumSatellites: p.Int64(6, "number of satellites"),
-		HDOP:          p.Float64(7, "hdop"),
-		Altitude:      p.Float64(8, "altitude"),
-		Separation:    p.Float64(10, "separation"),
-		DGPSAge:       p.String(12, "dgps age"),
-		DGPSId:        p.String(13, "dgps id"),
+		BaseSentence:     s,
+		Time:             p.Time(0, "time"),
+		Latitude:         p.LatLong(1, 2, "latitude"),
+		NorS:             p.String(2, "N or S"),
+		Longitude:        p.LatLong(3, 4, "longitude"),
+		EorW:             p.String(4, "E or W"),
+		FixQuality:       p.EnumString(5, "fix quality", Invalid, GPS, DGPS, PPS, RTK, FRTK, EST),
+		NumSatellites:    p.Int64(6, "number of satellites"),
+		HDOP:             p.Float64(7, "hdop"),
+		Altitude:         p.Float64(8, "altitude"),
+		AntennaMeters:    p.String(9, "Antenna height unit"),
+		Separation:       p.Float64(10, "separation"),
+		SeparationMeters: p.String(11, "Units of geoidal separation"),
+		DGPSAge:          p.String(12, "dgps age"),
+		DGPSId:           p.String(13, "dgps id"),
 	}, p.Err()
 }
